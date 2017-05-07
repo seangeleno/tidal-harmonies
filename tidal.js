cb06ebcd5c750af996a61d1e97a836b819f946d8
g wilken <greg@gwilken.com>
2017-05-06 21:10:21 -0700

    XML parser working for station data

--- tidal.js
+++ tidal.js
@@ -1,18 +1,39 @@
-  var soap = require('soap');
-  
-  var url = 'https://opendap.co-ops.nos.noaa.gov/axis/services/HarmonicConstituents?wsdl';
-  // var args = {
-  // 	name: 'value'
-  // };
-  
-  soap.createClient(url, function(err, client) {
+const soap = require('soap');
+const request = require('request');
+const xml2js = require('xml2js');
 
-  	if(err) console.log('error:', err);
+var url = 'https://opendap.co-ops.nos.noaa.gov/axis/services/HarmonicConstituents?wsdl';
+var stations = 'https://opendap.co-ops.nos.noaa.gov/stations/stationsXML.jsp';
 
-   	console.log('soap ok');
 
-   	var services = client.describe();
+var parser = new xml2js.Parser({explicitArray: false, mergeAttrs: true});
 
-   	console.log(JSON.stringify(services, null, 2));
 
-  });
+
+  // soap.createClient(url, function(err, client) {
+
+  // 	if(err) console.log('error:', err);
+
+  //  	console.log('soap ok');
+
+  //  	var services = client.describe();
+
+  //  	console.log(JSON.stringify(services, null, 2));
+
+  // });
+
+
+request(stations, function(err, res, body) {
+
+
+	parser.parseString(body, function(err, response) {
+
+		//console.log(JSON.stringify(response, null, 2));
+
+		console.log( JSON.stringify( response.stations.station[0], null, 2 ) );
+
+
+	})
+
+
+});
